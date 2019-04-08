7ac9bc99a8438463435f24da95cdabd09aeaee73
Thomas Campbell <thomascampbell1@gmail.com>
2019-04-08 15:03:01 -0600

    process working with call in main of PILLAGE

--- process_ransac.py
+++ process_ransac.py
@@ -1,85 +1,57 @@
 import numpy as np
-from matplotlib import pyplot as plt
 import csv
-import ransac
-from sklearn.cluster import DBSCAN
-from sklearn import metrics
+from RANSAC import ransac
 
-filepath = "./csv/train_0007.csv"
+def cluster(hit_data_in):
+    xyzs_data = [[] for ii in range(4)]
 
-x_data = []
-y_data = []
-z_data = []
-with open(filepath) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    n_true_protons = 0
-    n_features = -1
-    for iline, row in enumerate(csv_reader):
-        n_features = len(row)
-        n_features += -1
-        print("on event:", iline)
-        for ii, xx in enumerate(row):
-            if ii == 0:
-                n_true_protons = xx
-            elif ((ii-1)%4) == 0:
-                x_data.append(float(xx))
-            elif ((ii-1)%4) == 1:
-                y_data.append(float(xx))
-            elif ((ii-1)%4) == 2:
-                z_data.append(float(xx))
+    for ii, xx in enumerate(hit_data_in):
+        xyzs_data[ii%4].append(xx)
 
-        hit_data = [[],[],[]]
-        for ii in range(len(x_data)):
-            if not (x_data[ii] == 0 and y_data[ii] == 0 and z_data[ii] == 0):
-                hit_data[0].append(x_data[ii])
-                hit_data[1].append(y_data[ii])
-                hit_data[2].append(z_data[ii])
+    del_indecies = []
+    for ii in range(len(xyzs_data[3])):
+        if xyzs_data[0][ii] == 0 and xyzs_data[1][ii] == 0 and xyzs_data[2][ii] == 0 and xyzs_data[3][ii] == 0:
+            del_indecies.append(ii)
 
-        if len(hit_data[0]) < 5:
+    del_indecies.sort(reverse=True)
+
+    for dd in del_indecies:
+        for xyzs in xyzs_data:
+            del xyzs[dd]
+
+    if len(xyzs_data[3]) < 5:
+        return []
+
+
+    vikings = []
+    labels = ['X', 'Y', 'Z']
+    for ii in range(3):
+        for jj in range(3):
+            label = labels[ii] + labels[jj]
+            viking = ransac.viking(label)
+            viking.set_data(xyzs_data[ii],xyzs_data[jj])
+            viking.scale_data()
+            viking.ransack()
+            vikings.append(viking)
+
+    evt_labels = ransac.cluster_hits(vikings,xyzs_data)
+    n_clusters = -1
+    for ee in evt_labels:
+        if ee > n_clusters:
+            n_clusters = ee
+    n_clusters += 1
+
+    clusters = [ [] for ii in range(n_clusters)]
+    for ievt, ee in enumerate(evt_labels):
+        if ee < 0:
             continue
+        for ii in range(len(xyzs_data)):
+            clusters[int(ee)].append(xyzs_data[ii][ievt])
 
-        vikings = []
-        labels = ['X', 'Y', 'Z']
-        for ii in range(len(hit_data)):
-            for jj in range(len(hit_data)):
-                label = labels[ii] + labels[jj]
-                viking = ransac.viking(label)
-                viking.set_data(hit_data[ii],hit_data[jj])
-                viking.scale_data()
-                viking.ransack()
-                vikings.append(viking)
+    n_features = 1000
+    for ii in range(len(clusters)):
+        for jj in range(len(clusters[ii]), n_features):
+            clusters[ii].append(0)
 
-        evt_labels = ransac.cluster_hits(vikings,hit_data)
-        n_clusters = -1
-        for ee in evt_labels:
-            if ee > n_clusters:
-                n_clusters = ee
-        n_clusters += 1
-
-        clusters = [ [] for ii in range(n_clusters)]
-        for ievt, ee in enumerate(evt_labels):
-            if ee < 0:
-                continue
-            for ii in range(3):
-                clusters[int(ee)].append(hit_data[ii][ievt])
-
-        n_features = 1000
-        for ii in range(len(clusters)):
-            for jj in range(len(clusters[ii]), n_features):
-                clusters[ii].append(0)
-
-        #TODO: don't forget to write truth information back in here or account for it some other way
-        #TODO: will need to seperate by evt as well likely for actual use
-        #TODO: should re-create file somewhere upstairs to avoid accidental appends to exisiting old files
-        #TODO: carefully validate this after merge
-        with open('test.csv', mode='a') as outf:
-            out_writer = csv.writer(outf, delimiter=',')
-            for cc in clusters:
-                out_writer.writerow(cc)
-
-        for ii in range(len(hit_data)):
-            hit_data[ii].clear()
-
-        x_data.clear()
-        y_data.clear()
-        z_data.clear()
+    #TODO: need to re-sort by x and take another relative postion on these before returning them
+    return clusters
